5a58f5516d545e0d243ca6190b62cadddbd165fc
MateusPMG <86072940+MateusPMG@users.noreply.github.com>
2022-10-27 10:09:58 +0100

    commit 1

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 10:05:22 by marvin            #+#    #+#             */
+/*   Updated: 2022/10/27 10:05:22 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (dest);
+	d = dest;
+	s = src;
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dest);
+}
---
+++ ft_memmove.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 09:41:23 by marvin            #+#    #+#             */
+/*   Updated: 2022/10/27 09:41:23 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*s;
+	unsigned char		*d;
+
+	if (!dest && !src)
+		return (dest);
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	while (n--)
+		d[n] = s[n];
+	return (dest);
+}
