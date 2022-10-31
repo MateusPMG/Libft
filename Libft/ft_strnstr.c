053afb3b95442df6d0a025c793786f247b1c8d12
MateusPMG <mpmgboss@gmail.com>
2022-10-31 15:50:07 +0000

    commit 13 new files

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,11 +6,34 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 13:24:23 by mpatrao           #+#    #+#             */
-/*   Updated: 2022/10/31 13:34:36 by mpatrao          ###   ########.fr       */
+/*   Updated: 2022/10/31 15:49:04 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*strnstr(const char	*big, const char *little, size_t len)
 {
-	
+	int	i;
+	int	j;
+	int	k;
+
+	if (!little)
+		return (big);
+	i = 0;
+	while (big[i]++ && len--)
+	{
+		j = 0;
+		if (big[i] == little[j])
+		{
+			k = i;
+			while (big[k] == little[j])
+			{
+				k++;
+				j++;
+			}
+			if (little[j] == '\0')
+				return (&big[i]);
+		}
+		i++;
+	}
+	return (NULL);
 }
